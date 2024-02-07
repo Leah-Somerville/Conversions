58383a381edd2c0e4beb787c4b6ce804a5a1a477
Leah Somerville <leah.somerville4@gmail.com>
2024-02-07 09:33:16 -0600

    Initial Commit

---
+++ Conversions/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Conversions
+//
+//  Created by Leah Somerville on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
