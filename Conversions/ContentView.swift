e7b79b9a9a73d44be5e7c98aea203ee7a4321502
Leah Somerville <leah.somerville4@gmail.com>
2024-02-07 22:59:04 -0600

    Finished Project

--- Conversions/ContentView.swift
+++ Conversions/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var ogTemp: Double = 0.0
+    @State private var ogUnitSelected: String = "Fahrenheit"
+    @State var newUnitSelected: String = "Fahrenheit"
+    let unitSelection = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    var convertedTemp: Double {
+        var newTemp: Double = 0.0
+        
+        if ogUnitSelected == "Celsius" && newUnitSelected == "Celsius"{
+            newTemp = ogTemp
+        } else if ogUnitSelected == "Fahrenheit" && newUnitSelected == "Fahrenheit" {
+            newTemp = ogTemp
+        } else if ogUnitSelected == "Kelvin" && newUnitSelected == "Kelvin" {
+            newTemp = ogTemp
+        } else if ogUnitSelected == "Celsius" && newUnitSelected == "Fahrenheit"{
+            newTemp = (ogTemp - 32) * 5/9
+        } else if ogUnitSelected == "Celsius" && newUnitSelected == "Kelvin" {
+            newTemp = ogTemp + 273.15
+        } else if ogUnitSelected == "Fahrenheit" && newUnitSelected == "Celsius" {
+            newTemp = (ogTemp * 9/5) + 32
+        } else if ogUnitSelected == "Fahrenheit" && newUnitSelected == "Kelvin" {
+            newTemp = ((ogTemp - 32) * 5/9) + 273.15
+        } else if ogUnitSelected == "Kelvin" && newUnitSelected == "Celsius" {
+            newTemp = ogTemp - 273.15
+        } else if ogUnitSelected == "Kelvin" && newUnitSelected == "Fahrenheit" {
+            newTemp = (ogTemp - 273.15) * 1.8 + 32
+        } else {
+            newTemp = 99.99
         }
-        .padding()
+        
+        return newTemp
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section("Please Enter the Orginal Temperature"){
+                    TextField("Temperature", value: $ogTemp, format: .number)
+                    
+                    Picker("Temperature Unit", selection: $ogUnitSelected){
+                        ForEach(unitSelection, id: \.self){
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                Section("Please Enter the New Unit"){
+                    Picker("Temperature Unit", selection: $newUnitSelected){
+                        ForEach(unitSelection, id: \.self){
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section("New Temperature"){
+                    Text("\(convertedTemp.formatted())°")
+                }
+            }
+        }
     }
 }
 
